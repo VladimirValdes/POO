a5a3783d1b6b9b0c0ebaa09719c5bcf849ede3c1
Vladimir <mendozahum20@gmail.com>
2021-06-15 11:34:43 -0300

    herencia

---
+++ POO/Clases/Leon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO.Clases
+{
+    public class Leon: Carnivoro
+    {
+        public void corre()
+        {
+            Console.WriteLine("El leon corre");
+        }
+    }
+}
